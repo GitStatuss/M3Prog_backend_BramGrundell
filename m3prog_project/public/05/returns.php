2ccc82b0285be8b96be94d8d1938fba54bcc02e1
BramBoos469 <106348725+GitStatuss@users.noreply.github.com>
2025-03-18 11:32:30 +0100

    .

---
+++ m3prog_project/public/05/returns.php
@@ -0,0 +1,9 @@
+<?php
+
+function mijnFunction()
+{
+    //hier komt de code van de function
+    return 1;
+}
+
+?>
--- m3prog_project/public/05/zelfmetargumenten.php
+++ m3prog_project/public/05/zelfmetargumenten.php
@@ -8,6 +8,20 @@ $result = $euro * $yen;
 return $result;
 }
 $result = convertCurrency();
-print("euro: $result");
+print("euro: $result<br>");
+
+function sendEmail()
+{
+$mailTo = "pipipoepoe@gmail.com<br>";
+$mailFrom = "bramdepoepenkan@gmail.com<br>";
+$subject = "Pooping issue<br>";
+$message = "I really really have to poop right now, I need help";
+echo "$mailTo $mailFrom  $subject  $message";
+
+}
+echo 
+$yes = sendEmail();
+print("$yes");
+
 
 ?>
